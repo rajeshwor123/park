1453ba5974b082b5b0c4b7a05689a8cba374a369
unknown <niroularajeshwor@gmail.com>
2022-03-27 00:14:09 +0545

    profile icon designs

--- lib/model/profile_data.dart
+++ lib/model/profile_data.dart
@@ -3,11 +3,17 @@ class Profile{
   final String userName ;
   final String email;
   final String phone;
+  final double? latitude ;
+  final double? longitude ;
+  final String? bikePrice ;
+  final String? carPrice ;
+  final String? truckPrice ;
+  final String? isAvailable;
 
-  Profile(this.userName , this.email ,this.phone);
+  Profile(this.userName , this.email ,this.phone, this.latitude, this.longitude, this.bikePrice, this.carPrice, this.truckPrice, this.isAvailable);
 
   factory Profile.fromJson(Map<String,dynamic> data){
-    return  Profile(data['userName'],data['email'],data['phone']);
+    return  Profile(data['userName'],data['email'],data['phone'],data['latitude'],data['longitude'],data['bikePrice'],data['carPrice'],data['truckPrice'],data['isAvailable']);
   }
 
 }
--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:park/model/service.dart';
 import 'package:park/model/profile_data.dart';
 import 'package:flutter/material.dart';
@@ -13,23 +15,38 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
 
-  String userName = '';
-  String phone = '';
-  String email = '';
 
-  bool changeButton = false;
+  String userName = "";
+  String? email = "";
+  String phone = "";
+  double? latitude ;
+  double? longitude;
+  String? bikePrice = "";
+  String? carPrice = "";
+  String? truckPrice = "";
+  String? isAvailable = "";
   bool changeButton1 = false;
+  String? errorComment = "";
+  bool changeButton = false;
 
-  callLogin()async{
+
+  callDb()async{
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     if(sharedPreferences.getBool('session') == true) {
-      String? email = sharedPreferences.getString('email');
+      email = sharedPreferences.getString('email');
       String? password = sharedPreferences.getString('password');
       Profile profile = await Service.getProfile(email??"", password??"");
 
       setState(() {
         userName = profile.userName;
+        email = profile.email;
         phone = profile.phone;
+        bikePrice = profile.bikePrice;
+        carPrice = profile.carPrice;
+        truckPrice = profile.truckPrice;
+        isAvailable = profile.isAvailable;
+        latitude = profile.latitude;
+        longitude = profile.longitude;
       });
     }
     else{moveToLogin(context);}
@@ -56,6 +73,7 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   moveToChooseLoc(BuildContext context)async{
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     setState(() {
       changeButton1 = true;
     });
@@ -63,15 +81,19 @@ class _ProfilePageState extends State<ProfilePage> {
     await Navigator.pushNamed(context, PageRoutes.chooseLocRoute);
     if(mounted){
       setState(() {
+        latitude = sharedPreferences.getDouble("latitude");
+        longitude = sharedPreferences.getDouble("longitude");
         changeButton1 = false;
       });
     }
   }
 
+  String name = "";
+
   @override
   initState(){
     super.initState();
-    callLogin();
+    callDb();
   }
 
   @override
@@ -79,52 +101,94 @@ class _ProfilePageState extends State<ProfilePage> {
     return SafeArea(
       child: Material(
         child: Form(
-        child: Column(
-          children: [
-            Text(userName),
-            const SizedBox(height: 100.0),
-            Material(
-              color: Colors.cyan,
-              borderRadius: BorderRadius.circular(changeButton ? 20 : 10),
-              child: InkWell(
-                onTap: () => moveToChooseLoc(context),
-                child: AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  width: changeButton1 ? 300 : 150,
-                  height: changeButton1 ? 25 : 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "choose Location",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
-                  ),
-                ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 50),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  const Icon(Icons.person_rounded,size: 50,color: Colors.cyan),
+                  const SizedBox(width: 15),
+                  Text(userName??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                ],
               ),
-            ),
-            const SizedBox(
-              height: 50.0,
-            ),
+              const SizedBox(height: 10.0),
+              Row(
+                children: [
+                  const Icon(Icons.call_rounded,size: 50,color: Colors.greenAccent),
+                  const SizedBox(width: 15),
+                  Text(phone??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                children: [
+                  const Icon(Icons.directions_car_filled_rounded,size: 50,color: Colors.deepOrange),
+                  const SizedBox(width: 15),
+                  Text(carPrice??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                  const Spacer(),
+                  const Icon(Icons.delivery_dining_rounded,size: 50,color: Colors.indigo),
+                  const SizedBox(width: 15),
+                  Text(bikePrice??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                children: [
+                  const Icon(Icons.directions_bus_rounded,size: 50,color: Colors.amber),
+                  const SizedBox(width: 15),
+                  Text(truckPrice??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                  const Spacer(),
+                  const Icon(Icons.check_circle_rounded,size: 45,color: Colors.black87),
+                  const SizedBox(width: 15),
+                  Text(isAvailable??"", style: const TextStyle( fontWeight: FontWeight.bold)),
+                ],
+              ),
 
-            const SizedBox(height: 100,),
-            Material(
-              color: Colors.cyan,
-              borderRadius: BorderRadius.circular(changeButton ? 20 : 10),
-              child: InkWell(
-                onTap: () => moveToLogin(context),
-                child: AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  width: changeButton ? 300 : 100,
-                  height: changeButton ? 25 : 40,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    "logout",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+              const SizedBox(height: 100.0),
+              Material(
+                color: Colors.cyan,
+                borderRadius: BorderRadius.circular(changeButton ? 20 : 10),
+                child: InkWell(
+                  onTap: () => moveToChooseLoc(context),
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: changeButton1 ? 300 : 150,
+                    height: changeButton1 ? 25 : 40,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "choose Location",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 50.0,
+              ),
+
+              const SizedBox(height: 100,),
+              Material(
+                color: Colors.cyan,
+                borderRadius: BorderRadius.circular(changeButton ? 20 : 10),
+                child: InkWell(
+                  onTap: () => moveToLogin(context),
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: changeButton ? 300 : 100,
+                    height: changeButton ? 25 : 40,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      "logout",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ) ,
       ),
